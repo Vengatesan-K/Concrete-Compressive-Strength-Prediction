2df65a44d497ef2ba47594fba53d1b734243c6e9
Vengatesan-K <kannanvenkatesh772@gmail.com>
2024-03-10 17:29:14 +0530

    Eda and Model ipynb files uploaded

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info() # exc_tb = exception traceback
@@ -17,4 +17,5 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
     
