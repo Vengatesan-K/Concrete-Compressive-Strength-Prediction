0a6d492824ba5d660112c6f7a79429426f0266fa
Vengatesan-K <kannanvenkatesh772@gmail.com>
2024-03-12 18:46:01 +0530

    Create application.py

--- Strengths.py
+++ Strengths.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 
 class Strength(BaseModel):
+    Age : int
     Fly_Ash : float
     Average_Aggregate : float
     Water_Cement_Ratio : float
     Blast_Furnace_Slag : float
     Super_Plasticizer : float
-    Age : int
+    
